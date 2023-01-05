104cd7be02cd34084d8c1e2d97b07059eab4f5cb
1rodriguezfranco <98francorodriguez@gmail.com>
2023-01-05 20:04:38 -0300

    Subiendo proyecto MercadoLiebre

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log('Servidor corriendo en ' + port);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/login.html'));
+});
